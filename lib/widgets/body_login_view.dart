4237f3d1e1d8bd8e1a2e6574585568f68cc3df1e
Ahmed Ezz <ahmedezz9877@gmail.com>
2025-08-24 05:26:59 +0300

    Finish Header & Body Login View

--- lib/widgets/body_login_view.dart
+++ lib/widgets/body_login_view.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class BodyLoginView extends StatelessWidget {
   const BodyLoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        FittedBox(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(width: 16),
+              const Text(
+                'Welcome To Daily Plan',
+                style: TextStyle(color: Colors.white, fontSize: 26),
+              ),
+              const SizedBox(width: 6),
+              SvgPicture.asset(
+                'assets/images/waving_hand.svg',
+                width: 30,
+                height: 30,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 8),
+        const FittedBox(
+          child: Text(
+            'Your productivity journey starts here.',
+            style: TextStyle(color: Colors.white, fontSize: 22),
+          ),
+        ),
+        const SizedBox(height: 24),
+        SvgPicture.asset(
+          'assets/images/work_in_progress.svg',
+          width: 206,
+          height: 216,
+        ),
+      ],
+    );
   }
 }
